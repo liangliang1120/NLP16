665ff09a9665a4b335b300440588c2bd1b6eaee3
liangliang1120 <35073431+liangliang1120@users.noreply.github.com>
2020-01-20 21:59:59 +0800

    Create 理论问题.py

---
+++ 理论问题.py
@@ -0,0 +1,10 @@
+Why we need γ in reinforcement learning ?
+
+Please breifly explain what is value function and what is Q function ?
+
+How temperal difference related to dynamic programming and monte-carlo methods ?
+
+Please briefly describe what are value iteration and policy iteration ?
+
+How can we use deep lerning in reinforcement learning ?
+
